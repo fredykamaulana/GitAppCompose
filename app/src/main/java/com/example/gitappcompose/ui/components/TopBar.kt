efe755941070408ac8a88225ee39d2e61fcd56a5
Fredyka Maulana <fredyka.maulana@gmail.com>
2023-05-17 11:18:48 +0700

    change topBar composable behavior

--- app/src/main/java/com/example/gitappcompose/ui/components/TopBar.kt
+++ app/src/main/java/com/example/gitappcompose/ui/components/TopBar.kt
@@ -1,38 +1,23 @@
 package com.example.gitappcompose.ui.components
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.gitappcompose.R
 import com.example.gitappcompose.ui.theme.GitAppComposeTheme
 
 @Composable
 fun TopBar(
     modifier: Modifier = Modifier,
-    navigateUp: () -> Unit,
+    navigationIcon: @Composable () -> Unit,
     title: @Composable () -> Unit,
     actions: @Composable RowScope.() -> Unit
 ) {
 
     TopAppBar(
         modifier = modifier,
-        navigationIcon = {
-            IconButton(onClick = {
-                navigateUp()
-            }) {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = stringResource(id = R.string.menu_back)
-                )
-            }
-        },
+        navigationIcon = navigationIcon,
         title = title,
         actions = actions
     )
@@ -44,7 +29,7 @@ fun TopBarPreview() {
     GitAppComposeTheme {
         TopBar(
             title = {},
-            navigateUp = {},
+            navigationIcon = {},
             actions = {}
         )
     }
