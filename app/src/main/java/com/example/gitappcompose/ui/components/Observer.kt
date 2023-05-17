dc550211dace52cd1c1e1d848841f777966abc7e
Fredyka Maulana <fredyka.maulana@gmail.com>
2023-05-17 11:19:45 +0700

    change parameter resource

--- app/src/main/java/com/example/gitappcompose/ui/components/Observer.kt
+++ app/src/main/java/com/example/gitappcompose/ui/components/Observer.kt
@@ -7,8 +7,8 @@ import com.example.gitappcompose.data.util.Result
 fun observeSetFavourite(context: Context, result: Result<Long>, setFavouriteAction: () -> Unit) {
     when(result){
         is Result.Success -> {
-            if(result.data == -1L) showToast(context, context.getString(R.string.favourite_save_conflict_message))
-            else showToast(context, context.getString(R.string.favourite_save_success_message))
+            if(result.data == -1L) showToast(context, R.string.favourite_save_conflict_message)
+            else showToast(context, R.string.favourite_save_success_message)
 
             setFavouriteAction.invoke()
         }
@@ -21,7 +21,7 @@ fun observeDeleteFavourite(context: Context, result: Result<Int>, deletedAction:
     when(result){
         is Result.Success -> {
             deletedAction.invoke()
-            showToast(context, context.getString(R.string.favourite_delete_success_message))
+            showToast(context, R.string.favourite_delete_success_message)
         }
         is Result.Error -> { showToast(context, result.errorMessage.orEmpty()) }
         else -> {}
